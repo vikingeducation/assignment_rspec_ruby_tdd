49c94695ce8da686dfda03567ee9e0717d34d999
Michael Alexander <alexandermw@gmail.com>
2014-11-06 14:01:22 -0800

    Update tower_spec.rb

--- spec/tower_spec.rb
+++ spec/tower_spec.rb
@@ -2,9 +2,23 @@ require 'lib/tower'
 
 
 describe Game do
+  #need a board double and a player double, with stubs for 
   it 'accepts a variable number of disks'
-  it 'continues the game until a player wins or quits'
-  it 'renders the game status at the beginning of each turn'
+  
+  describe '#play' do
+
+    
+    it 'renders the game status at the beginning of each turn'
+    it 'asks player to #take_turn each iteration of the turn loop'
+    it 'loops another turn as long as NOT #game_over?'
+    
+  end
+  
+  describe '#game_over' do
+    it 'returns true if player quits'
+    it 'returns true if player has won'
+    it 'returns false if player has not quit and has not won'
+  end
 end
 
 describe Board do
@@ -44,6 +58,12 @@ describe Board do
   
   describe '#valid_move?' do
     it 'does not allow placing a larger disk onto a smaller disk'
+    
+    it 'takes an array of two integers'
+    
+    it 'moves the top disk from the tower indicated by the first integer to the second'
+    
+    it 'uses ordinals, not cardinals'
   end
   
   describe '#render' do
@@ -54,6 +74,6 @@ end
 
 describe Player do
   describe '#get_move' do
-    it 'allows the player to quit at any time by entering q'
+    it 'allows the player to quit at any time by entering "q" '
   end
 end
