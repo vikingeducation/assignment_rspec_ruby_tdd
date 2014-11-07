9413965343306c5ebd5dd992ba3547269f2aa4ee
Andur Carr <carr.andur@gmail.com>
2014-11-07 12:01:25 -0800

    Finish anagram tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,11 @@
 def anagram(word)
   raise ArgumentError, "Must use a string" unless word.is_a?(String)
-  word = word.split(' ').first.downcase
-  File.open('TWL06.txt')
-  []
+  word = word.split(' ').first.upcase
+  anagrams = []
+  File.open('TWL06.txt').each do |entry|
+    if entry.strip.split('').sort == word.split('').sort && entry.strip != word
+      anagrams << entry.strip
+    end
+  end
+  anagrams
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,33 +1,42 @@
 require 'anagram'
 
 describe '#anagram' do
+  before(:each) do
+    allow(File).to receive(:open).and_return(["LOOTER", "RETOOL", "ROOTLE", "TOOLER", "LIFE"])
+  end
+
   it "should take a one-word string input" do
-    expect{anagram('HIJACK')}.not_to raise_error
+    expect{anagram('LOOTER')}.not_to raise_error
   end
 
   it "should accept lower-case words" do
-    expect(anagram('Competence')).to eq(anagram('COMPETENCE'))
+    expect(anagram('Looter')).to eq(anagram('LOOTER'))
   end
 
   it "should only use the first word given" do
-    expect(anagram('COMPETENCE FLYING')).to eq(anagram('COMPETENCE'))
+    expect(anagram('LOOTER FILE')).to eq(anagram('LOOTER'))
   end
 
   it "should load the dictionary" do
-    allow(File).to receive(:open)
     expect(File).to receive(:open)
-    anagram("COMPETENCE")
+    anagram("LOOTER")
   end
 
   context 'return' do
     it "should return an array of anagrams if there are any" do
-      allow(File).to receive(:open).and_return([""])
+      expect(anagram("LOOTER")).to include("RETOOL")
     end
 
-    it "should not return the original word"
+    it "should not return the original word" do
+      expect(anagram("LOOTER")).not_to include("LOOTER")
+    end
 
-    it "should return an exhaustive list"
+    it "should return an exhaustive list" do
+      expect(anagram("LOOTER")).to eq(["RETOOL", "ROOTLE", "TOOLER"])
+    end
 
-    it "should return an empty array if there are no anagrams"
+    it "should return an empty array if there are no anagrams" do
+      expect(anagram("SUPERFLUOUS")).to eq([])
+    end
   end
 end
