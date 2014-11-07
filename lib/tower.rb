bd49c28aa1923725955469212bbae361f716b078
Michael Alexander <alexandermw@gmail.com>
2014-11-06 22:53:27 -0800

    game finished

---
+++ lib/tower.rb
@@ -0,0 +1,117 @@
+require 'pry'
+
+class Game
+  def initialize(height = 3)
+    @board = Board.new(height)
+    @player = Player.new(@board)
+  end
+
+  def play
+    loop do
+      @board.render
+      @player.take_turn
+      break if game_over?
+    end
+
+    endgame
+  end
+
+  private
+
+  def endgame
+    if @board.victory?
+      puts "You won!!!"
+      @board.render
+    end
+    puts "Thanks for playing!"
+  end
+
+  def game_over?
+    @player.quitting? || @board.victory?
+  end
+end
+
+class Board
+  attr_reader :towers
+  NUM_TOWERS = 3
+
+  def initialize( height = 3 )
+    raise "height must be an integer!" unless height.is_a?(Integer)
+
+    @height = height
+    @towers = Array.new(NUM_TOWERS) { Array.new }
+    set_first_tower(@height)
+  end
+
+  def victory?
+    towers[1].size == @height || towers[2].size == @height
+  end
+
+  def render
+    (@height-1).downto(0) do |level|
+      towers.each {|t| print (t[level] || " ").ljust(@height) }
+      puts "\n"
+    end
+  end
+
+  def make_move(move)
+    return false unless valid?(move)
+    first, second = move[0], move[1]
+    towers[second].push(towers[first].pop)
+  end
+
+
+  private
+
+  def valid?(move)
+    return false unless well_formed?(move) && in_range?(move)
+    first, second = move[0], move[1]
+    return false if towers[first].empty?
+    towers[second].empty? || towers[first][-1] < towers[second][-1]
+  end
+
+  def well_formed?(move)
+    move.all?{|m| m.is_a? Integer} && move.size == 2
+  end
+
+  def in_range?(move)
+    move[0].between?(1, NUM_TOWERS) && move[1].between?(1, NUM_TOWERS)
+  end
+
+  # creates pyramid on first tower up to specified height.
+  def set_first_tower(height)
+    height.downto(1) { |width| @towers[0] << "o" * width }
+  end
+
+
+
+end
+
+class Player
+
+  def initialize(board)
+    @quitting = false
+    @board = board
+  end
+
+  def take_turn
+    move = nil
+
+    loop do
+      print "Input your move: "
+      move = gets.chomp.split(",")
+
+      @quitting = true if move[0] =~ (/[qQ]/)
+      move.map!{ |x| x.to_i }
+
+      break if @board.make_move(move) || quitting?
+    end
+
+  end
+
+  def quitting?
+    @quitting
+  end
+
+end
+
--- spec/tower_spec.rb
+++ spec/tower_spec.rb
@@ -1,79 +1,143 @@
-require 'lib/tower'
+require 'tower'
+require 'pry'
 
 
 describe Game do
   #need a board double and a player double, with stubs for 
-  it 'accepts a variable number of disks'
-  
-  describe '#play' do
 
-    
-    it 'renders the game status at the beginning of each turn'
-    it 'asks player to #take_turn each iteration of the turn loop'
-    it 'loops another turn as long as NOT #game_over?'
-    
+  describe '#initialize' do
+
+
+    it 'creates a Board of specified size' do
+      expect(Board).to receive(:new).with(5)
+      Game.new(5)
+    end
+
   end
-  
-  describe '#game_over' do
-    it 'returns true if player quits'
-    it 'returns true if player has won'
-    it 'returns false if player has not quit and has not won'
+
+  describe '#play' do
+    let(:board){ double(:render => nil, :victory? => true) }
+    let(:player){ double(:quitting? => true, :take_turn => nil) }
+
+    before do
+      allow(Player).to receive(:new).and_return(player)
+      allow(Board).to receive(:new).and_return(board)
+    end
+
+    it 'renders the game status at the beginning of each turn' do
+      expect(board).to receive(:render)
+      subject.play
+    end
+
+    it 'asks player to #take_turn each iteration of the turn loop' do
+      expect(player).to receive(:take_turn)
+      subject.play
+    end
+
+    it 'thanks the player if the game is over' do
+      allow(subject).to receive(:game_over?).and_return(true)
+      expect{subject.play}.to output(/[Tt]hank.+ for playing/).to_stdout
+    end
+
+    it 'celebrates your victory' do
+      allow(subject).to receive(:game_over?).and_return(true)
+      expect{subject.play}.to output(/[Yy]ou won/).to_stdout
+    end
+
+    it 'skips the victory message if you quit' do
+      allow(subject).to receive(:game_over?).and_return(true)
+      allow(board).to receive(:victory?).and_return(false)
+      expect{subject.play}.not_to output(/[Yy]ou won/).to_stdout
+    end
   end
 end
 
 describe Board do
 
   describe '#towers' do
-  
+
     it 'is an array' do
       expect(subject.towers).to be_a(Array)
     end
-    
-    it 'starts with a full first tower and two empty towers, each of which are arrays'
+
+    it 'starts with a full first tower and two empty towers, all arrays' do
       expect(subject.towers).to eq( [ ["ooo","oo","o"], [], [] ] )
     end
-    
-    it 'accepts a variable number of disks' do 
-      let(:four_high){ Board.new(4) }
-      expect(four_high.towers).to eq( ( [ ["oooo","ooo","oo","o"], [], [] ] )
+
+    it 'accepts a variable number of disks' do
+      expect(Board.new(4).towers).to eq( [ ["oooo","ooo","oo","o"], [], [] ] )
     end
-    
+
     it 'only accepts integer numbers of disks' do
-      expect(Board.new(3.5)).to raise_error
+      expect{Board.new(3.5)}.to raise_error
     end
-    
+
     it 'cannot be modified via setters' do
       expect{subject.towers = [["o"],[],[]]}.to raise_error(NoMethodError)
     end
-    
+
   end
-  
+
 
   describe '#victory?' do
-    it 'is true when the tower is fully assembled on a different peg than the initial one'
-      allow(subject).to_receive(:towers).and_return( [ [], [], ["ooo","oo","o"] ] )
+    it 'is true when the tower is fully assembled on a different peg than the initial one' do
+      allow(subject).to receive(:towers).and_return( [ [], [], ["ooo","oo","o"] ] )
       expect(subject.victory?).to equal(true)
     end
   end
-  
-  describe '#valid_move?' do
-    it 'does not allow placing a larger disk onto a smaller disk'
-    
-    it 'takes an array of two integers'
-    
-    it 'moves the top disk from the tower indicated by the first integer to the second'
-    
-    it 'uses ordinals, not cardinals'
+
+  describe '#make_move' do
+    it 'takes an array of two integers' do
+      expect{subject.make_move([1,2])}.not_to raise_error
+    end
+
+    it 'returns false unless given an array of two integers' do
+      expect(subject.make_move(["q"])).to equal(false)
+    end
+
+    it 'uses ordinals, not cardinals' do
+      expect(subject.make_move([0,1])).to equal(false)
+    end
+
+    it 'does not allow placing a larger disk onto a smaller disk' do
+      allow(subject).to receive(:towers).and_return([ ["oo", "o"],["ooo"],[] ])
+      expect(subject.make_move([2, 1])).to equal(false)
+    end
+
   end
-  
+
   describe '#render' do
-    
+    it 'renders the board' do
+      expect{subject.render}.to output(/o\s+oo\s+ooo/).to_stdout
+    end
   end
-  
+
 end
 
 describe Player do
-  describe '#get_move' do
-    it 'allows the player to quit at any time by entering "q" '
+  let(:board){instance_double("Board")}
+  let(:subject){Player.new(board)}
+
+  before do
+    allow(board).to receive(:make_move).with(["q"]).and_return(:false)
+  end
+
+  describe '#take_turn' do
+    before do
+      allow(board).to receive(:make_move).with(anything)
+    end
+
+    it 'gets a move from the player' do
+      allow(board).to receive(:make_move).with([1,3]).and_return(:true)
+      allow(subject).to receive(:gets).and_return("1,3")
+      expect(subject).to receive(:gets)
+      subject.take_turn
+    end
+
+    it 'allows the player to quit by entering "q" ' do
+      allow(subject).to receive(:gets).and_return("q\n")
+      subject.take_turn
+      expect(subject.quitting?).to equal(true)
+    end
   end
 end
