85654d77db0bca47ac593df7ca9286339b8e93ba
niconan <nick.hax@gmail.com>
2015-04-10 14:16:18 -0700

    add anagrams and spec

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,39 +1,24 @@
 class Anagrams
 
-  attr_accessor :word
+  attr_reader :word
 
   def initialize( word )
+    raise ArgumentError, "Bad input!" unless word.is_a?(String)
     @word = word
   end
 
   def find_anagrams
     anagram_matches = []
-    # for each permutation of the individual characters in the word string,
     @word.chars.permutation do |word_permutation|
-      # set the permutation equal to the joined permutation so the characters aren't separated
-      # and downcased so capitalization isn't an issue
-      word_permutation = word_permutation.join.downcase
-      File.open("/path/to/dictionary.txt") do |file| # open the dictionary file
-        file.any? do |word_in_dict| # .any? makes us read lines from the file until the end (nil char)
-          # if the stripped word on our current line (stripped b/c newline char)
-          # is equal to the input word, go to next block, we don't want it
-          if word_in_dict.strip == word
-            next
-          # elsif the stripped word on our current line
-          # is equal to the current permutation of our given word
-          elsif word_in_dict.strip == word_permutation
-            anagram_matches << word_permutation # shovel the current permutation into matches
-          end
+      word_permutation = word_permutation.join.upcase
+      File.open("path/to/dictionary.txt").each do |word_in_dict|
+        if word_in_dict.strip.upcase == word
+          next
+        elsif word_in_dict.strip.upcase == word_permutation
+          anagram_matches << word_permutation
         end
       end
     end
-    puts "#{anagram_matches}"
+    anagram_matches.uniq
   end
-
 end
-
-anagrams( "pears" ) # test case for lowercase word
-puts ""
-anagrams( "PEARS" ) # test case for uppercase word
-puts ""
-anagrams( "zygote" ) # test case for word w/ no anagrams
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,3 +1,31 @@
-describe Anagram do
+require 'anagrams'
 
+describe Anagrams do
+  before(:each) { allow(File).to receive(:open).and_return(["RETOOL", "ROOTLE", "TOOLER", "NICEMEME"])}
+  let(:anagrams) { Anagrams.new("looter") }
+  describe '#initialize' do
+    it 'gets the word' do
+      expect(anagrams.word).to eq("looter")
+    end
+    let(:bad_anagrams) { Anagrams.new(1) }
+    it 'raises an ArgumentError for non-string input' do
+      expect { bad_anagrams }.to raise_error(ArgumentError)
+    end
+  end
+  describe '#find_anagrams' do
+    it 'opens the file' do
+      expect(File).to receive(:open)
+      anagrams.find_anagrams
+    end
+    it 'returns an array' do
+      expect(anagrams.find_anagrams).to be_a(Array)
+    end
+    let(:no_anagrams) { Anagrams.new("zebra") }
+    it 'returns a blank array if no valid anagrams' do
+      expect(no_anagrams.find_anagrams).to eq([])
+    end
+    it 'returns an array with valid anagrams' do
+      expect(anagrams.find_anagrams).to include("RETOOL", "ROOTLE", "TOOLER")
+    end
+  end
 end
