d33b8fb562562cd2629ba398ce3d1149f02db773
Garrett <garrettweinert@gmail.com>
2015-07-03 16:56:30 -0700

    Finished TDD warmup tests

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -1,10 +1,13 @@
+
+
 class TestDrivenDevelopment
+  
   def stock_picker(arr)
     profit = 0
     ans = []
     arr.each_with_index do |price,index|
       for i in (index+1)...arr.length
-
+          next if price > arr[i]
            temp_profit = arr[i] - price
            if temp_profit  > profit 
                ans = [index, i] 
@@ -15,4 +18,18 @@ class TestDrivenDevelopment
     return ans
   end
   
+  def anagrams(string)
+    return_array=[]
+    dictionary = File.readlines("anyfilename.txt")
+    string.downcase!
+    string.strip!
+    dictionary.each do |line|  
+      if string.chars.sort == line.chars.sort
+        return_array.push(line)
+      end
+    end
+    return_array
+  end
+
+
 end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -2,8 +2,10 @@ require './lib/tdd'
 
 
 describe TestDrivenDevelopment do
-  describe '#stock_picker' do
-    let(:tdd){TestDrivenDevelopment.new}
+  
+  let(:tdd){TestDrivenDevelopment.new}
+  
+  describe '#stock_picker' do  
 
     it 'returns an array of numbers' do
 
@@ -28,5 +30,62 @@ describe TestDrivenDevelopment do
     it 'returns empty if array has all equal numbers' do
       expect(tdd.stock_picker([5,5,5,5,5,5,5])).to be_empty
     end
+
   end
+
+  describe '#anagrams' do
+
+    before(:each) do
+      allow(File).to receive(:readlines).and_return(["retool",
+       "rootle", "tooler"])
+    end
+
+    it 'returns an array' do
+      # dub_dict = double(:readlines => "looter")
+      
+      expect(tdd.anagrams("looter")).to be_a(Array)
+    end
+
+    it 'returns a correct array if string is "looter"' do
+      expect(tdd.anagrams("looter")).to eq(["retool",
+       "rootle", "tooler"])
+
+    end
+
+    it 'returns an empty array if no anagrams found' do
+      expect(tdd.anagrams("zygote")).to be_empty
+    end
+
+    it 'returns correct array if string has uppercase' do
+      expect(tdd.anagrams("LooTer")).to eq(["retool",
+       "rootle", "tooler"])
+    end
+
+    it 'returns correct array if string has whitespace/newlines' do
+      expect(tdd.anagrams("    LooTer \n")).to eq(["retool",
+       "rootle", "tooler"])
+    end
+
+
+  end
+
+
+
 end
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
