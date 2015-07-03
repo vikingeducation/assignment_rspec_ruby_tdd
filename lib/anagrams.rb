a97d4823d28ae497cee5530e360e37c40af27b19
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-03 18:52:18 -0400

    anagrams added

---
+++ lib/anagrams.rb
@@ -0,0 +1,8 @@
+def anagrams(word)
+
+  raise "Input not a string" if word.class != String
+  raise "String must be only one world" if word.include?(" ")
+
+  output = []
+
+end
---
+++ spec/anagrams_spec.rb
@@ -0,0 +1,22 @@
+require 'anagrams'
+
+describe '#anagrams' do
+  
+  it "should raise an error if input is not string" do
+    expect{anagrams(1)}.to raise_error
+  end
+
+  it "should accept a string" do
+    expect{anagrams("hello")}.not_to raise_error
+  end
+
+  it "should raise an error if string contains spaces" do
+    word = "hello world"
+    expect{anagrams(word)}.to raise_error
+  end
+
+  it "should return an array" do
+    expect(anagrams("hello").class).to be(Array)
+  end
+
+end
