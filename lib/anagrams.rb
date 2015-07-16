d357dca7cdc4bc046772904924a4c4584c9fa2d9
Justin Mullis <justin.mullis@gmail.com>
2015-07-15 20:25:31 -0400

    completed TDD warmup

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,6 +1,18 @@
 def anagrams(word)
   raise ArgumentError unless word.is_a?(String)
-  # dict = IO.new
-  # dict.read("dict.txt")
-  # dict.split!
+  raise ArgumentError if word.include?(" ")
+
+  dict = IO.read("dict.txt")
+  dict.map! { |i| i.strip }
+
+  anagrams = []
+  word = word.split("").to_a
+  word.permutation.to_a.each do |v|
+    if dict.include?(v.join) && word != v &&
+       !anagrams.include?(v.join) 
+        anagrams << v.join 
+    end
+  end
+
+  return anagrams
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,14 +1,33 @@
 require 'anagrams'
 
 describe "anagrams method" do
-  #let(:dict) { double("IO", :read => ["blah", "blah", "blah"]) }
+  let(:dictionary) { ["retool\n", "rootle\n", "tooler\n", "anchor\n", 
+                      "shrubs\n", "looter\n"] }
   
   it "requires a string for input" do
     expect{ anagrams(1337) }.to raise_error(ArgumentError)
   end
 
-  it "should load a dictionary" do
-    #allow(IO).to receive(:read).with("dict.txt").and_return(["blah\n", "blah\n", "blah\n"])
-    expect( anagrams("words") ).to eq("whatever")
+  it "requires a single word for input" do
+    expect{ anagrams("Bill Shakespeare") }.to raise_error(ArgumentError)
+  end
+
+  it "loads a dictionary" do
+    expect( IO ).to receive(:read).with("dict.txt").and_return(dictionary)
+    anagrams("test")
+
+    # dictionary = ["blah\n", "blah\n", "blah\n"]
+    # expect( IO ).to receive(:read).with("dict.txt").and_return(dictionary)
+    # anagrams("test")
+  end
+
+  it "returns an array of anagrams" do
+    allow( IO ).to receive(:read).with("dict.txt").and_return(dictionary)
+    expect( anagrams("looter") ).to match_array( ["retool", "rootle", "tooler"] )
+  end
+
+  it "returns an empty array if there are no anagrams" do
+    allow( IO ).to receive(:read).with("dict.txt").and_return(dictionary)
+    expect( anagrams("zygote") ).to eq([])
   end
 end
