c13f828759d79231099164abda94550e61b98413
leedu708 <djl265@cornell.edu>
2015-08-13 17:42:03 -0400

    completed stock_picker_spec.rb

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,13 +1,27 @@
-class StockPicker
+def stock_picker(array)
+  raise("input is not an array") unless array.is_a?(Array)
+  raise("input is too small") unless array.length > 2
+  raise("input must contain only integers") unless array.all? { |element| element.is_a?(Integer) }
 
-  attr_reader :pick_stock
+  profit = 0
+  final = []
 
-  def initialize(array = nil)
-    @pick_stock = array
+  array.each_with_index do |price, buy_day|
+    buy_day.upto(array.length - 1) do |sell_day|
+
+      if array[sell_day] > price
+        current_profit = array[sell_day] - price
+
+        if current_profit > profit
+          profit = current_profit
+          final = [buy_day, sell_day]
+        end
+
+      end
+
+    end
   end
 
-  def stock_picker(array = @pick_stock)
-    final = []
-  end
+  final
 
 end
--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -1,10 +1,29 @@
 require 'stock_picker'
 
-describe StockPicker do
+describe '#stock_picker' do
 
-  it 'takes an array' do
-    test = StockPicker.new([1, 2, 3])
-    expect(test.pick_stock).to eq([1, 2, 3])
+  it 'raises an error if input is not an array' do
+    expect{stock_picker('s')}.to raise_error("input is not an array")
+  end
+
+  it 'raises an error if input does not have at least 3 elements' do
+    expect{stock_picker([1, 2])}.to raise_error("input is too small")
+  end
+
+  it 'raises an error if input array does not fully comprise of integers' do
+    expect{stock_picker(['a', 2, 3])}.to raise_error("input must contain only integers")
+  end
+
+  context 'returns the two days with greatest profit' do
+
+    it 'can return greatest days when prices are in order' do
+      expect(stock_picker([1, 2, 3, 4, 5, 6, 7])).to eq([0, 6])
+    end
+
+    it 'can return greatest days when prices are out of order' do
+      expect(stock_picker([3, 2, 1, 4, 5, 6, 7])).to eq([2, 6])
+    end
+
   end
 
 end
