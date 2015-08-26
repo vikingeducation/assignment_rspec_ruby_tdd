90aa2d245e20c9ea30999a48df03b07736b1c0e2
Chris Scavello <bideowego@gmail.com>
2015-08-26 01:47:24 -0400

    Fix bug

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,6 +1,6 @@
 def stock_picker(prices)
 	raise "Argument must be an array" unless prices.is_a?(Array)
-	raise "Not a number" unless prices[0].is_a?(Numeric)
+	raise "Not a number" unless prices.all? {|p| p.is_a? Numeric}
 	max_profit = max_profit_day_one = max_profit_day_two = 0
 	(0...prices.length).each do |i|
 		((i + 1)...prices.length).each do |j|
