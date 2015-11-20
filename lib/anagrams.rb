38b6c37d8b593fb46bbd9162a295e97225cfbcf0
Sia <siakaramalegos@gmail.com>
2015-11-20 10:54:33 -0600

    Adds anagram code and bundles

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,3 +1,14 @@
 def anagrams(string)
-  []
+  # Load dictionary
+  # Can compress - check if length equal first then if sorted word equals sorted current word in dictionary
+  alpha_string = string.chars.sort.join
+  anagrams = []
+  File.readlines("enable.txt").each do |readline|
+    r = readline.chomp
+    anagrams << r if r.chars.sort.join == alpha_string
+  end
+
+  anagrams
 end
+
+# puts anagrams("pears").to_s
