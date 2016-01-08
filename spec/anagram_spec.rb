6ce298909430b5a2d4907c9282b8f14c0147409d
Jeffrey Gisin <jgisin@gmail.com>
2016-01-08 16:18:35 -0700

    exception testing

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,7 +1,16 @@
 require 'anagrams'
 
 describe "Anagrams" do 
+  let(:anagram){ Anagrams.new }
 
-  
+
+  describe "#anag" do
+
+    it "checks that a string is passed" do
+        expect{anagram.ana(1)}.to raise_error("String not passed")
+        expect{anagram.ana([1,2])}.to raise_error("String not passed")
+    end
+
+  end
 
 end
