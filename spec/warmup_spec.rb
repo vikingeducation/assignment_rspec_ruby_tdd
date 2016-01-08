9f2c13463dcd9348343d7e7ca18a1399a776f995
koziscool <kozjob@yahoo.com>
2016-01-08 13:15:56 -0600

    a test or two

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -9,6 +9,13 @@ describe "warmup tests"  do
       double( size: 3 )
     end
 
+    let(:some_string) {  "tetris" } 
+
+    let(:loud_string) do 
+      double("String", reverse!: true  )
+    end
+
+
     describe "#get_shout" do
 
         it "takes a string and returns uppercase" do
@@ -27,5 +34,24 @@ describe "warmup tests"  do
       end
 
     end
+
+  
+
+    describe "#calls_some_methods" do
+
+      it "ensures that upcase! is called" do
+        expect(some_string).to receive(:upcase!).and_return("TETRIS")
+        warmup.calls_some_methods(some_string)
+      end
+
+      # it "ensures that upcase! is called" do
+      #   allow(some_string).to receive(:upcase!).and_return("TETRIS")
+      #   warmup.calls_some_methods(some_string)
+      # end
+
+
+
+    end
+
 end
 
