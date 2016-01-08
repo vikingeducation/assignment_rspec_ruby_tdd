f520298df3a9c8c4d2bad42e3c18f47d0f892b0c
Jeffrey Gisin <jgisin@gmail.com>
2016-01-08 15:41:11 -0700

    added more checks to input

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -4,6 +4,7 @@ class StockPicker
     raise "Array not passed" if not array.is_a? Array
     raise "Array elements should be numeric and positive" if not array.all? {|item| item.is_a? Numeric}
     raise "Array elements should be numeric and positive" if not array.all? {|item| item >= 0}
+    raise "Array size shuold be a minimum of 2" if array.length < 2
   end
 
 end
