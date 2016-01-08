dfff1f9a1b3b0ad0b39725b70990dfbb3f91eb67
Thomas Lo <thomasjinlo@gmail.com>
2016-01-08 09:56:44 -0800

    Finished triple_size

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -4,6 +4,11 @@ require_relative "../lib/warmup"
 describe "warmup tests"  do 
     
     let( :warmup ) { Warmup.new }
+
+    let(:fake_array) do 
+      double( size: 3 )
+    end
+
     describe "#get_shout" do
 
         it "takes a string and returns uppercase" do
@@ -12,9 +17,15 @@ describe "warmup tests"  do
             # expect(warmup.gets_shout).to eq("SHOUT")
         end
 
-
-
     end
 
+
+    describe "#triple_size" do
+
+      it "takes in a double that accepts size method" do
+        expect(warmup.triple_size(fake_array)).to eq(9)
+      end
+
+    end
 end
 
