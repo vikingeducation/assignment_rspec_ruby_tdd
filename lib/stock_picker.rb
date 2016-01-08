f72ff2ef30d7ad61c177d1659295c531dc8e4792
Jeffrey Gisin <jgisin@gmail.com>
2016-01-08 15:37:51 -0700

    added more checks to input

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -2,6 +2,8 @@ class StockPicker
 
   def pick(array)
     raise "Array not passed" if not array.is_a? Array
+    raise "Array elements should be numeric and positive" if not array.all? {|item| item.is_a? Numeric}
+    raise "Array elements should be numeric and positive" if not array.all? {|item| item >= 0}
   end
 
 end
