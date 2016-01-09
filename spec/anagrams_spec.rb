6d4f7b96061f5531ec8fa4618ea9a595f52a8665
Andrew Baik <and.baik@gmail.com>
2016-01-08 20:41:04 -0500

    anagram filereader specs

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,8 +1,22 @@
 require 'anagrams'
 
 describe '#anagrams' do
+
+	let(:my_file_double) { double("File") }
+
+	before(:each) do
+		allow(File).to receive(:open).and_return(my_file_double)
+		allow(my_file_double).to receive(:readlines).and_return( ["a", "b", "c"]  )
+		allow(my_file_double).to receive(:close)
+	end
+
 	it 'takes a one word string' do
 		expect{ anagrams(12) }.to raise_error(ArgumentError)
+	end
+
+	it 'properly obtains readlines' do
+		expect(my_file_double).to receive(:readlines).and_return( ["c", "b", "a"] )
+		anagrams("string")
 	end
 
 	it 'checks if split is called' do
@@ -11,9 +25,5 @@ describe '#anagrams' do
 		anagrams(string)
 	end
 
-	it 'properly loads dictionary' do
-		expect(File).to receive(:readlines).and_return(["s", "t", "r", "i", "n", "g"])
-	  anagrams("st")
-	end
 
 end
