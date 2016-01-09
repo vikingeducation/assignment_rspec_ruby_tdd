203b8e08c8d5645d83c7940f23a73174d4689a8d
Deepa Kackar <dkackar99@yahoo.com>
2016-01-08 17:31:05 -0800

    added more tests in game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,11 +3,12 @@ require 'game'
 
 describe "Game" do 
   let(:game){ Game.new }
+  let(:game2){ Game.new(3) }
   # let(:file){ File.new }
 
   describe "check that game is initialized" do
     it "initializes the Dice object" do
-        expect(game.dice.class).to eq(Dice)
+        expect(game.dice).to eq(1)
     end 
 
     it "initializes the Human object" do
@@ -18,5 +19,8 @@ describe "Game" do
         expect(game.computer.class).to eq(Computer)
     end 
 
+    it "initializes dice correctly when no of dice is passed" do
+        expect(game2.dice).to eq(3)
+    end 
   end
 end   
