40c03fc138e32081c8f2ea733e934cf7fd39751a
Deepa Kackar <dkackar99@yahoo.com>
2016-01-08 17:24:58 -0800

    added more tests in game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,15 +7,15 @@ describe "Game" do
 
   describe "check that game is initialized" do
     it "initializes the Dice object" do
-        expect(game.dice.is_a?).to eq(Dice)
+        expect(game.dice.class).to eq(Dice)
     end 
 
     it "initializes the Human object" do
-        expect(game.human.is_a?).to eq(Human)
+        expect(game.human.class).to eq(Human)
     end 
 
     it "initializes the Computer object" do
-        expect(game.computer.is_a?).to eq(Computer)
+        expect(game.computer.class).to eq(Computer)
     end 
 
   end
