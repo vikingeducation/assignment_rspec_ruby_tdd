6c5690038600711ba87c74769a08abe8d2d6ec88
Stephen Mayeux <stephenmayeux@gmail.com>
2016-05-11 01:03:08 +0900

    dice throw pseudocode

---
+++ lib/game.rb
@@ -0,0 +1,46 @@
+class Game
+
+  # intialize game
+    # new human player
+    # new computer player
+
+  # #start method
+    # game instructions
+    # render scores
+    # start loop
+      # get human input
+        # validate input
+      # get computer input
+      # generate dice rolls
+      # increment scores
+      # render scores
+
+  # def render_score
+
+  # generate random dice rolls
+    # pass rolls to increment scores
+
+  # increment_score
+
+
+end
+
+class Human
+
+  # initialize
+    # score
+
+  # def get_input
+    # unless valid_input?
+      # get_input
+
+end
+
+class Computer
+
+  # initilaize
+    # score
+
+  # computer_input
+
+end
