59163f3233ab99c2722daebcd974a8139baa4c3f
Joe Bernardi <joebernardi@hotmail.com>
2016-07-10 18:42:45 -0500

    add init tests to game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,15 +8,58 @@ describe '.Game' do
 	# initialize player and computer
 	let( :game ) { Game.new }
 
+	describe '#initialize' do
+
+		it 'should set current player to player' do
+
+			expect( game.instance_variable_get( :@current_player ) ).to be_a( Player )
+
+		end
+
+
+		it 'computer should be a Computer' do
+
+			expect( game.instance_variable_get( :@computer) ).to be_a( Computer )
+
+		end
+
+		it 'player should be a Player' do
+
+			expect( game.instance_variable_get( :@player) ).to be_a( Player )
+
+		end
+
+		it 'board should be a Board' do
+
+			expect( game.instance_variable_get( :@board ) ).to be_a( Board )
+
+		end
+
+		it '@player_score set to 0' do
+
+			expect( game.instance_variable_get( :@player_score )).to eq( 0 )
+
+		end
+
+		it '@cpu_score set to 0' do
+
+			expect( game.instance_variable_get( :@cpu_score )).to eq( 0 )
+
+		end
+
+	end
+
 	describe '#change_players' do
 
 		it 'should change players' do
 
-			instance_variable_set( :@current_player ).with :@player
+			player = Player.new
 
-			expect( instance_variable_get( :@current_player )).to be_a( Computer )
+      game.instance_variable_set( :@current_player, player )
 
 			game.change_players
+
+			expect( game.change_players ).to be_a( Computer )
 
 
 		end
@@ -35,18 +78,34 @@ describe '.Game' do
 
 			end
 
+			it 'should add to player score if player wins round' do
+
+				game.check_result( 6, 5 )
+				game.check_result( 6, 5 )
+				# score is 0 2
+				game.check_result( 6, 5 )
+
+				expect( game.instance_variable_get( :@player_score ) ).to eq( 3 )
+
+			end
+
+
+			it 'should add to computer score if player wins round' do
+
+				game.check_result( 3, 5 )
+				game.check_result( 3, 5 )
+				# score is 0 2
+				game.check_result( 3, 5 )
+
+				expect( game.instance_variable_get( :@cpu_score ) ).to eq( 3 )
+
+			end
+
 		end
+
 
 
 	end
 
-	# assigns total
-	it 'should assign the sum to the current player'
-	# checks win
-	it 'should choose the player if their total is higher'
-	it 'should choose the cpu if their total is higher'
-	# increments score
-	it 'should add one to the score for player winning that round'
-	# increment round
 
 end
