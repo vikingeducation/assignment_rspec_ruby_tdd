7b1005c975adab057c6526d81082de1e69957ae8
essian <jessicampdennis@hotmail.com>
2016-07-11 23:42:10 +0100

    adds tests for stockpicker

--- lib/examples.rb
+++ lib/examples.rb
@@ -3,11 +3,16 @@ class Examples
   def stock_picker(arr)
     raise ArgumentError unless arr.class == Array && arr.all? { |item| item.class == Fixnum } && arr.length >= 2
     max = 0
-    result = []
-    arr.each_with_index do |day_one, index|
-      arr.each_with_index do |day_two, index|
-        max = day_two - day_one if day_two - day_one > max
+    result = [0,0]
+    arr.each_with_index do |day_one, index1|
+      arr.each_with_index do |day_two, index2|
+        if index2 > index1 && day_two - day_one > max
+            max = day_two - day_one
+            result[0], result[1] = index1, index2
+        end
       end
     end
+    return result unless result==[0,0]
+    puts "bad time to trade"
   end
 end
--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -18,10 +18,26 @@ describe Examples do
       expect(subject.stock_picker([1,2])).to be_a(Array)
     end
     it "returns an array with length of two" do
-      expect(subject.stock_picker([1,2].length)).to be_a(2)
+      expect(subject.stock_picker([1,2]).length).to be(2)
     end
     it "returns the best two days to trade" do
       expect(subject.stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])).to eq([2, 6])
+    end
+    it "outputs a message if there's no increase in price" do 
+      expect(subject).to receive(:puts)
+      subject.stock_picker([1,1,1])
+    end
+    it "returns nil if there's no increase in price" do
+      expect(subject.stock_picker([3,2,1])).to be_nil
+    end
+  end
+
+  describe "#anagrams" do
+    it "takes two arguments" do
+      expect{subject.anagrams('', []).not_to raise_error}
+    end
+    it "takes a string and an array as arguments" do 
+
     end
   end
 
