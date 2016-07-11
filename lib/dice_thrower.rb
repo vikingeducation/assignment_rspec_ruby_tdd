a145f08b927c4eafc60cee488889df98efcd9780
Leo Ahnn <leoahnn@gmail.com>
2016-07-11 19:59:49 -0400

    worked on board spec and code

--- lib/dice_thrower.rb
+++ lib/dice_thrower.rb
@@ -6,10 +6,12 @@ require 'view'
 
 class DiceThrower
 
+  def initialize
+    @player1 = Human.new
+    @player2 = Player.new
+    @board = Board.new([@player1, @player2])
+  end
 
 
-def dice_thrower
-
-end
 
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,11 @@
  class Player
+   attr_reader :roll
+
+   def initialize
+     @roll = []
+   end
+   
    def roll_dice
-
-
 
    end
 
