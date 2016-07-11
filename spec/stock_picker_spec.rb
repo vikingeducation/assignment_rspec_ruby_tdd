bad5171425b702d3ff4af5b1478d0d9dbb8462e2
Leo Ahnn <leoahnn@gmail.com>
2016-07-11 18:20:30 -0400

    initialized rspec + gemfile

---
+++ spec/stock_picker_spec.rb
@@ -0,0 +1,13 @@
+require 'stockpicker'
+
+describe "stockpicker" do
+  let(:days){[44, 30, 24, 32, 35, 30, 40, 38, 15]}
+  it 'returns most profitable days for buying then selling' do
+    expect
+  end
+  it 'throws an error if input is not an array'
+  it 'throws an error if any inputs in array are not a number'
+  it 'throws an error if '
+
+
+end
