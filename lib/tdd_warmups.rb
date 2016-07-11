15a992e664286c01ea7ecf387d8766daa42657c6
cjvirtucio87 <cjv287@gmail.com>
2016-07-11 15:42:04 -0700

    What does it mean to check for the most profitable day?

---
+++ lib/tdd_warmups.rb
@@ -0,0 +1,6 @@
+def stock_picker(array)
+  raise "Cannot pass empty array." if array.empty?
+  raise "Cannot pass array with one element." if array.length == 1
+  array.each_index do |i|
+  end
+end
--- spec/tdd_warmups_spec.rb
+++ spec/tdd_warmups_spec.rb
@@ -1,20 +1,30 @@
-require 'tdd_warmups_spec'
+require 'tdd_warmups'
 
-describe "StockPicker" do
+describe "#stock_picker" do
+  let(:argument) { [44, 30, 24, 32, 35, 30, 40, 38, 15] }
 
   it "takes an array as an argument" do
+    expect(argument).to be_a(Array)
   end
 
   it "raises an error when the argument is empty" do
+    empty_arr = []
+    expect{ stock_picker(empty_arr) }.to raise_error("Cannot pass empty array.")
   end
 
   it "raises an error when the array has only one stock price" do
+    one_stock = [1]
+    expect{ stock_picker(one_stock) }.to raise_error("Cannot pass array with one element.")
   end
 
   it "conducts a search on the array argument" do
+    expect(argument).to receive(:each_index)
+    stock_picker(argument)
   end
 
   it "checks each day for the most profitable one" do
+    
+    #compare_prices(buff,curr)
   end
 
   it "it bases the state of being profitable on the 
