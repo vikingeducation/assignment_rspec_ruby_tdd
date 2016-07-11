5b388dbd8cc02d8d5fb33ec17d39368b78ca495b
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-11 19:01:35 -0400

    Start method stock_picker

--- lib/tdd_warmups.rb
+++ lib/tdd_warmups.rb
@@ -1,3 +1,9 @@
 def stock_picker(array)
   raise ArgumentError if array.class != Array
+  profitable_days = []
+
+  array.each_with_index do |element1, index1|
+    index1.upto(array.length - 1) do |index2|
+    end
+      else
 end
