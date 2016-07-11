bbc532e15056ae88965b472a19ddec8478e6d202
Alex Lach <alexglach@gmail.com>
2016-07-11 18:15:11 -0400

    getting further on stock picker

--- lib/warmups.rb
+++ lib/warmups.rb
@@ -1,10 +1,19 @@
 def stock_picker(input)
   raise ArgumentError unless input.is_a?(Array) && input.length > 1
   raise ArgumentError unless input.all? { |item| item.is_a?(Fixnum) }
-  buy_day, sell_day = [nil, nil]
-  input.each_with_index do |price, day|
-
+  buy_day, sell_day = nil, nil
+  input.each_with_index do |buy_price, first_day|
+    buy_day == nil ?  buy_day = first_day : buy_day
+    input.each_with_index do |sell_price, second_day|
+      sell_day == nil ? sell_day = second_day : second_day
+      if sell_price - buy_price > input[sell_day] - input[buy_day]
+        buy_day = first_day
+        sell_day = second_day
+      end
+    end
   end
+  raise "There are no profits to be made here" unless input[sell_day] > input[buy_day]
+  [buy_day, sell_day]
 
 end
-raise "There are no profits to be made here"
+
--- spec/warmups_spec.rb
+++ spec/warmups_spec.rb
@@ -17,4 +17,8 @@ describe '#stock_picker' do
   it 'raises an error if array is the same throughout' do
     expect{stock_picker([1, 1, 1, 1, 1])}.to raise_error("There are no profits to be made here")
   end
+
+  it 'returns the most profitable days of an array' do
+    expect(stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])).to eq([2,6])
+  end
 end
