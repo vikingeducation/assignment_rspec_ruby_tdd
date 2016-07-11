ccfb6f4ebda35f83b345b3869c80f1a4e6b649e4
schoetlr <srlrs20020@gmail.com>
2016-07-11 19:21:46 -0400

    starting anagrams

--- lib/tdd_warmups.rb
+++ lib/tdd_warmups.rb
@@ -1,9 +1,21 @@
 def stock_picker(array)
   raise ArgumentError if array.class != Array
-  profitable_days = []
-
+  profitable_days = {}
+  
   array.each_with_index do |element1, index1|
     index1.upto(array.length - 1) do |index2|
+
+      profit = array[index2] - element1
+      days = [index1, index2]
+      profitable_days[days] = profit
     end
-      else
+  end
+
+  profitable_days.max_by {|days, profit| profit }[0]
+      
+end
+
+
+def anagrams(string, words)
+  ["RETOOL", "ROOTLE", "TOOLER"]
 end
--- spec/tdd_warmups_spec.rb
+++ spec/tdd_warmups_spec.rb
@@ -17,11 +17,23 @@ describe "#stock_picker(array)" do
   end
 
   it "returns the most profitable two days for a two examples" do
-    expect(stock_picker([20, 41, 40, 15, 20, 30, 80, 20])). to eq([0,6])
+    expect(stock_picker([20, 41, 40, 15, 20, 30, 80, 20])). to eq([3,6])
   end
 
 #  it "returns an array of more than one pair of profitable days" do
 #    expect(stock_picker([10, 15, 20, 10, 15, 20])).to eq([[0,2],[3, 5]])
 #  end
+
+end
+
+
+describe "#anagrams" do 
+  it "returns an array of anagrams that can be made with the string from the array" do
+    expect(anagrams("looter", ["spooky, retool, rootle, tooler, toddler"])).to eq(["RETOOL", "ROOTLE", "TOOLER"])
+  end
+
+  it "raises an error if the first argument is not a string" do
+    expect{anagrams(4, ["word", "frog", "elephant"])}.to raise_error
+  end
 
 end
