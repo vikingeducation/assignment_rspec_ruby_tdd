4487efec1a767cab6eb695e162ec96f82b8e9b0b
David Watts <davidjeremywatts@gmail.com>
2016-07-11 15:42:59 -0700

    finish stock_picker

--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -1,13 +1,31 @@
-require 'stockpicker'
+require 'stock_picker'
 
 describe "stockpicker" do
-  let(:days){[44, 30, 24, 32, 35, 30, 40, 38, 15]}
+  let(:days1){[44, 30, 24, 32, 35, 30, 40, 38, 15]}
+  let(:days2){[2, 8, 2, 8, 2, 8, 2, 8]}
+
   it 'returns most profitable days for buying then selling' do
-    expect
+    expect(stockpicker(days1)).to eq([2, 6])
   end
-  it 'throws an error if input is not an array'
-  it 'throws an error if any inputs in array are not a number'
-  it 'throws an error if '
+
+  it 'returns earliest pair of optimal days' do
+    expect(stockpicker(days2)).to eq([0, 1])
+  end
+
+  it 'throws an error if input is not an array' do
+    arr = String.new
+  expect{stockpicker(arr)}.to raise_error(ArgumentError)
+  end
+
+  it 'throws an error if any inputs in array are not a number' do
+    arr = [4, 6, "a"]
+  expect{stockpicker(arr)}.to raise_error(ArgumentError)
+  end
+
+  it 'throws an error if input array has less than 2 entries' do
+    arr = [4]
+  expect{stockpicker(arr)}.to raise_error(ArgumentError)
+  end
 
 
 end
