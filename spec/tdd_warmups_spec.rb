6b21b77451974cdf98fec6cfd858944d595742d8
cjvirtucio87 <cjv287@gmail.com>
2016-07-11 15:27:04 -0700

    High-level design.

---
+++ spec/tdd_warmups_spec.rb
@@ -0,0 +1,31 @@
+require 'tdd_warmups_spec'
+
+describe "StockPicker" do
+
+  it "takes an array as an argument" do
+  end
+
+  it "raises an error when the argument is empty" do
+  end
+
+  it "raises an error when the array has only one stock price" do
+  end
+
+  it "conducts a search on the array argument" do
+  end
+
+  it "checks each day for the most profitable one" do
+  end
+
+  it "it bases the state of being profitable on the 
+      difference between sale price on a day and buying 
+      price on another" do
+  end
+
+  # it "test 1: checks for the most profitable day for buying and selling stock" do
+  # end
+
+  # it "test 2: checks for the most profitable day for buying and selling stock" do
+  # end
+
+end
