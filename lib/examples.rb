9ed58c0196006bab022ece7103db9a71a64aa470
Graham Turner <turnertgraham@gmail.com>
2016-07-11 18:17:04 -0400

    working on stock_picker method round two

--- lib/examples.rb
+++ lib/examples.rb
@@ -3,8 +3,10 @@ class Examples
   def stock_picker(arr)
     raise ArgumentError unless arr.class == Array && arr.all? { |item| item.class == Fixnum } && arr.length >= 2
     max = 0
+    result = []
     arr.each_with_index do |day_one, index|
       arr.each_with_index do |day_two, index|
+        max = day_two - day_one if day_two - day_one > max
       end
     end
   end
