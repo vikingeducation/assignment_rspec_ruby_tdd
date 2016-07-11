556c9862211c690de6cb0d94e8f239a108ee0071
Vishal Lama <vishal.lama@gmail.com>
2016-07-11 17:32:52 -0600

    stock_picker

--- lib/tdd_warmups.rb
+++ lib/tdd_warmups.rb
@@ -1,6 +1,20 @@
 def stock_picker(array)
   raise "Cannot pass empty array." if array.empty?
   raise "Cannot pass array with one element." if array.length == 1
+  
+  max_value = 0
+  max_value_index = 0
+  min_value_index = 0
+
   array.each_index do |i|
+    j = i
+    while j < array.length do
+      compare_prices(buffer, array[j])
+    end
   end
 end
+
+def compare_prices(buffer, current)
+  
+end
+
--- spec/tdd_warmups_spec.rb
+++ spec/tdd_warmups_spec.rb
@@ -3,10 +3,6 @@ require 'tdd_warmups'
 describe "#stock_picker" do
   let(:argument) { [44, 30, 24, 32, 35, 30, 40, 38, 15] }
 
-  it "takes an array as an argument" do
-    expect(argument).to be_a(Array)
-  end
-
   it "raises an error when the argument is empty" do
     empty_arr = []
     expect{ stock_picker(empty_arr) }.to raise_error("Cannot pass empty array.")
@@ -14,7 +10,8 @@ describe "#stock_picker" do
 
   it "raises an error when the array has only one stock price" do
     one_stock = [1]
-    expect{ stock_picker(one_stock) }.to raise_error("Cannot pass array with one element.")
+    expect{ stock_picker(one_stock) }
+      .to raise_error("Cannot pass array with one element.")
   end
 
   it "conducts a search on the array argument" do
@@ -23,14 +20,12 @@ describe "#stock_picker" do
   end
 
   it "checks each day for the most profitable one" do
-    
-    #compare_prices(buff,curr)
+    expect(Object).to receive(:compare_prices)
   end
 
   it "it bases the state of being profitable on the 
       difference between sale price on a day and buying 
-      price on another" do
-  end
+      price on another"
 
   # it "test 1: checks for the most profitable day for buying and selling stock" do
   # end
