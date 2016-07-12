b696fff421d387c968e53aa282f8480ae523fbc8
Morgan Martin <morganm5201@gmail.com>
2016-07-11 20:00:08 -0400

    Add get_player_dice spec

--- lib/warmups.rb
+++ lib/warmups.rb
@@ -1,19 +1,33 @@
 def stock_picker(input)
   raise ArgumentError unless input.is_a?(Array) && input.length > 1
   raise ArgumentError unless input.all? { |item| item.is_a?(Fixnum) }
-  buy_day, sell_day = nil, nil
-  input.each_with_index do |buy_price, first_day|
-    buy_day == nil ?  buy_day = first_day : buy_day
-    input.each_with_index do |sell_price, second_day|
-      sell_day == nil ? sell_day = second_day : second_day
-      if sell_price - buy_price > input[sell_day] - input[buy_day]
-        buy_day = first_day
-        sell_day = second_day
+  buy_day_pick = 0
+  sell_day_pick = 0
+  profit = 0
+  buy_day = 0
+  while buy_day < input.length
+    sell_day = buy_day + 1
+    while sell_day < input.length
+      combo_profit = input[sell_day] - input[buy_day]
+      if combo_profit >profit
+        profit = combo_profit
+        buy_day_pick = buy_day
+        sell_day_pick = sell_day
       end
+      sell_day += 1
     end
+    buy_day += 1
   end
-  raise "There are no profits to be made here" unless input[sell_day] > input[buy_day]
-  [buy_day, sell_day]
-
+  raise "There are no profits to be made here" unless input[sell_day_pick] > input[buy_day_pick]
+  [buy_day_pick, sell_day_pick]
 end
 
+
+def anagrams(string, array)
+  raise ArgumentError unless string.is_a?(String) && array.is_a?(Array)
+  raise ArgumentError unless array[0].is_a?(String) && array.length == 1
+  new_array = array[0].split(', ')
+  new_array.select do |word|
+    word.chars.sort == string.chars.sort
+  end.map { |word| word.upcase }
+end
