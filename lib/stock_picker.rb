1f72e0bc261b798daec2c4aaf4e3bc44125aee11
BranLiang <lby89757@hotmail.com>
2016-07-30 12:54:34 +0800

    finish the stock picker

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,5 +1,19 @@
 def stock_picker stocks = []
-  raise 'Input data should be an array!' unless
+  raise 'Input data should be an array!' unless stocks.is_a? Array
+  raise 'Input data should be positive!' unless stocks.all? { |price| price > 0 }
   return 'No pairs available!' if stocks.empty? || stocks.sort.reverse == stocks
+  pairs = []
+  max_difference = 0
+  stocks.each_with_index do |buy_price, buy_day|
+    stocks.each_with_index do |sell_price, sell_day|
+      price_difference = sell_price - buy_price
+      if price_difference > max_difference && buy_day < sell_day
+        max_difference = price_difference
+        pairs[0] = buy_day
+        pairs[1] = sell_day
+      end
+    end
+  end
+  pairs
 
 end
--- spec/stcok_picker_spec.rb
+++ spec/stcok_picker_spec.rb
@@ -16,8 +16,23 @@ describe 'stockpicker' do
     expect(stock_picker [30, 20]).to eq(warning)
   end
 
+  it 'return no pairs available when input has only one data' do
+    expect(stock_picker [10]).to eq(warning)
+  end
+
   it 'raise warning when input is not an array' do
     expect{ stock_picker 'string' }.to raise_error('Input data should be an array!')
+  end
+
+  it 'raise warning when input has negative values' do
+    expect{ stock_picker [10, -10, 30] }.to raise_error('Input data should be positive!')
+  end
+
+  it 'return [0, 1] when inputs is [10, 20]' do
+    expect(stock_picker [10, 20]).to eq([0, 1])
+  end
+  it 'return [2, 6] when input is [44, 30, 24, 32, 35, 30, 40, 38, 15]' do
+    expect(stock_picker [44, 30, 24, 32, 35, 30, 40, 38, 15]).to eq([2, 6])
   end
 
 
