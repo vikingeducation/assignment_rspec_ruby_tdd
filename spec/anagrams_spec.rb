29302435e363710c6dbbce14bee85c94e2596896
Jonathan Popenuck <john.popenuck@gmail.com>
2016-11-04 15:25:47 -0700

    anagrams start

---
+++ spec/anagrams_spec.rb
@@ -0,0 +1,17 @@
+require 'anagrams'
+
+describe '#anagrams' do
+
+  it 'raises an error if not given a word'
+
+  it 'raises an error if not given a dictionary'
+
+  it 'returns a list of matching anagrams'
+
+  it 'does not return the original word you sent it'
+
+  it 'only will only find anagrams for a single word'
+
+
+
+end
