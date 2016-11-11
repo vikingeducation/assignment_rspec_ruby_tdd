ffed40578c847f4b8953c331c8705da15cbc988f
¨frendo <geewatt@gmail.com>
2016-11-11 15:10:27 +0000

    structure rspec for warmups

---
+++ spec/human_spec.rb
@@ -0,0 +1,12 @@
+require 'dice_thrower'
+
+describe 'Human'
+  describe '#initialize' do
+    it "inherits player's #initialize method"
+  end
+
+  describe '#continue?' do
+    it 'returns player input to continue or quit' do
+
+    end
+  end
