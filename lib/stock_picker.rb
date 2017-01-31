79e6780cf37ec00d32bab89c463e84311ebffcb3
andie5 <andieasa@googlemail.com>
2017-01-31 23:13:41 +0000

    stock picker tests

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -17,6 +17,11 @@ def stock_picker(stock_prices)
     if(price < min && (i < len-2))
       min = price
       days[0] = i
+
+      # # If we find a new minimum day price, the maximum needs to be after this
+
+      max = stock_prices[i+1]
+      days[1] = i+1
     end
 
     # If we have found a new maximum stock price save the day
@@ -30,5 +35,5 @@ def stock_picker(stock_prices)
 
 end
 
-stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])
+# stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])
 #=> [2, 6]
--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -22,10 +22,9 @@ describe 'stock_picker' do
     expect(stock_picker([1,2]).size).to eq(2)
   end
 
-  it 'The 2nd day to be more than the 1st stock day' do
+  it 'The 2nd day should be more than the 1st stock day' do
     stock_days = [2,3,4,10]
     result = stock_picker(stock_days)
-
     expect(result[0]).to be < result[1]
   end
 
@@ -43,7 +42,12 @@ describe 'stock_picker' do
   end
 
   it 'returns the maximum day' do
+    days = stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])
+      expect(days[1]).to eq(6)
   end
 
-
+  it 'returns the min and max' do
+    days = stock_picker([20, 78])
+    expect(days).to eq([0,1])
+  end
 end
