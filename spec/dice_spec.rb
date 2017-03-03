8d2b5dd0a0cbd64399f3a8cedba69469a36b0701
Roy Chen <roychenlei@gmail.com>
2017-03-03 13:59:55 +0800

    added additional tests for #roll method

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -15,6 +15,13 @@ describe "Dice" do
   describe "#roll" do
     it "returns a value from 1 to 6" do
       expect([1, 2, 3, 4, 5, 6]).to include(dice.roll)
+
+      test_rolls = []
+      100000.times do
+        test_rolls.push(dice.roll)
+      end
+
+      expect(test_rolls.all? { |roll| [1, 2, 3, 4, 5, 6].include?(roll) })
     end
   end
 end
