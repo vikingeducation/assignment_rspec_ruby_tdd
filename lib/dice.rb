61b1cae9d9c7c78e3454920fedc1bda20fb18de8
Roy Chen <roychenlei@gmail.com>
2017-03-03 13:21:04 +0800

    added specs and implementation for Dice class

---
+++ lib/dice.rb
@@ -0,0 +1,7 @@
+module DiceGame
+  class Dice
+    def roll
+      (1..6).to_a.sample
+    end
+  end
+end
---
+++ spec/dice_spec.rb
@@ -0,0 +1,20 @@
+# spec/dice_spec.rb
+
+require 'dice'
+include DiceGame
+
+describe "Dice" do
+  let (:dice) { Dice.new }
+
+  describe "#initialize" do
+    it "creates an instance of Dice" do
+      expect(dice).to be_a(Dice)
+    end
+  end
+
+  describe "#roll" do
+    it "returns a value from 1 to 6" do
+      expect([1, 2, 3, 4, 5, 6]).to include(dice.roll)
+    end
+  end
+end
