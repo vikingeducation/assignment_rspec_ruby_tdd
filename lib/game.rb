abff0c9e2d759a7cbf70578f9f4a5174e8a52873
Roy Chen <roychenlei@gmail.com>
2017-03-05 06:13:13 +0800

    specs and implementation for Game class so far

---
+++ lib/game.rb
@@ -0,0 +1,55 @@
+require 'dice'
+require 'human'
+require 'computer'
+
+module DiceThrower
+  class Game
+    attr_reader :human,
+                :computer,
+                :human_score,
+                :computer_score
+
+    def initialize(human = nil, computer = nil)
+      @human = human.nil? ? Human.new : human
+      @computer = computer.nil? ? Computer.new : computer
+      @human_score = 0
+      @computer_score = 0
+    end
+
+    def play
+      print_instructions
+
+      human_input = self.human.get_input
+    end
+
+    def increment_human_score
+      @human_score += 1
+    end
+
+    def increment_computer_score
+      @computer_score +=1 
+    end
+
+    def tie?(human_roll, computer_roll)
+      human_roll == computer_roll
+    end
+
+    def over?
+      @human.get_input == 'q'
+    end
+
+    def quit
+      puts "Goodbye!"
+      exit
+    end
+
+    def print_instructions
+      puts "Welcome to Dice Thrower!"
+      puts "Instructions:"
+      puts "Enter how many dice you'd like to throw."
+      puts "If your total is higher than the computer, you win!"
+      puts "Enter 'q' to quit."
+      puts
+    end
+  end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,96 @@
+# spec/game_spec.rb
+
+require 'game'
+include DiceThrower
+
+describe "Game" do
+  let (:game) { Game.new }
+
+  describe "#initialize" do
+    it "creates an instance of Game" do
+      expect(game).to be_a(Game)
+    end
+
+    it "creates an instance of Human in the Game instance" do
+      expect(game.human).to be_a(Human)
+    end
+
+    it "creates an instance of Computer in the Game instance" do
+      expect(game.computer).to be_a(Computer)
+    end
+
+    it "sets the Human's score to 0" do
+      expect(game.human_score).to eq(0)
+    end
+
+    it "sets the Computer's score to 0" do
+      expect(game.computer_score).to eq(0)
+    end
+  end
+
+  describe "human" do
+    it "cannot be modified after it's set on initialize" do
+      expect { game.human = nil }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe "computer" do
+    it "cannot be modified after it's set on initialize" do
+      expect { game.computer = nil }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe "human_score" do
+    it "cannot be modified publically after it's set on initialize" do
+      expect { game.human_score += 1}.to raise_error(NoMethodError)
+    end
+  end
+
+  describe "computer_score" do
+    it "cannot be modified publically after it's set on initialize" do
+      expect { game.computer_score += 1}.to raise_error(NoMethodError)
+    end
+  end
+
+  describe "#increment_human_score" do
+    it "increases the Human's score by one" do
+      game.increment_human_score
+      expect(game.human_score).to eq(1)
+    end
+  end
+
+  describe "increment_computer_score" do
+    it "increases the Computer's score by one" do
+      game.increment_computer_score
+    end
+  end
+
+  describe "#play" do
+  end
+
+  describe "#tie?" do
+    it "returns true if the Human's roll and Computer's roll are equal" do
+      human_double = instance_double("Human", roll_dice: 18)
+      computer_double = instance_double("Computer", roll_dice: 18)
+      game = Game.new(human_double, computer_double)
+
+      expect(game.tie?(human_double.roll_dice, computer_double.roll_dice)).to be_truthy
+    end
+  end
+
+  describe "#over?" do
+    it "returns true if the Human player has entered 'q'" do
+      # using a double
+      human_double = instance_double("Human", get_input: 'q')
+      
+      game = Game.new(human_double)
+      expect(game.over?).to be_truthy
+    end
+  end
+
+  describe "#quit" do
+    it "quits the game" do
+      expect { game.quit }.to raise_error(SystemExit)
+    end
+  end
+end
