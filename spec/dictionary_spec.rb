be893564d80567145560cbd90bba4a06f1298e24
Ann Allan <ann.cristina.allan@gmail.com>
2017-03-28 19:26:02 -0400

    Add solution and specs for Dictonary

---
+++ spec/dictionary_spec.rb
@@ -0,0 +1,12 @@
+require 'dictionary'
+describe 'dictionary' do
+  it 'returns array of anagrams that can be made from the specified string and are included in the dictionary' do
+    allow(File).to receive(:readlines).and_return(%w(spooky retool rootle tooler toddler))
+    expect(anagrams("looter")).to eq(["retool", "rootle", "tooler"])
+  end
+  it 'returns an empty array if not anagrams on the word list can be made' do
+    allow(File).to receive(:readlines).and_return(%w(spooky retool rootle tooler toddler))
+    expect(anagrams("zygote")).to eq([])
+  end
+
+end
