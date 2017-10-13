94de6327be0ea8fb3fa7b94a357f7a8d641ff5e2
mac718 <mac718@gmail.com>
2017-10-13 11:43:47 -0400

    working method achieved

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,5 +1,22 @@
 def stock_picker(prices)
-  raise "nope" unless prices.is_a?(Array)
   raise "nope" unless prices.size > 1
   raise "nope" if prices.sort { |a, b| b <=> a } == prices
+
+  if prices[0] == prices.min
+    return [0, prices.max.index]
+  else
+    max_diff = 0
+    best_days = [nil, nil]
+    1.upto(prices.size - 2) do |beg_index|
+      beg_index.upto(prices.size - 1) do |end_index|
+        if prices[end_index] - prices[beg_index] > max_diff
+          max_diff = prices[end_index] - prices[beg_index]
+          best_days = [beg_index, end_index]
+        end
+      end
+    end
+        
+  end
+  best_days
+
 end
--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -3,7 +3,7 @@ require 'stock_picker'
 describe 'stock_picker' do
 
   it 'raiese and error when non-Array is passed in' do 
-    expect{stock_picker('hello')}.to raise_error(RuntimeError)
+    expect{stock_picker('hello')}.to raise_error(NoMethodError)
   end
 
   it 'raises an error if passed-in Array has fewer than two elements' do 
@@ -12,5 +12,9 @@ describe 'stock_picker' do
 
   it "raises an error if the Array's values are descending" do 
     expect{stock_picker([20, 15, 10])}.to raise_error(RuntimeError)
+  end
+
+  it 'returns Array of indeces of passed-in Array indicating the best days to buy and sell stock' do 
+    expect(stock_picker([44, 30, 24, 32, 35, 30, 40, 38, 15])).to eql([2, 6])
   end
 end
